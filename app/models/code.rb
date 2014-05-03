aca1bc91a98d755ead0697ac5b2fe9c894dcf823
Adrian Rangel <adrian.rangel@gmail.com>
2014-05-03 01:56:54 -0500

    move submit to Code class

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,4 +1,15 @@
 class Code < ActiveRecord::Base
   validates_uniqueness_of :titulo
   validates_presence_of :titulo, :codigo
+
+  def submit(input="")
+    input.to_file("in.txt")
+    codigo.to_file("codigo.cpp")
+    system("g++ codigo.cpp -o a.out")
+    out = `./a.out < in.txt`
+    system("rm a.out")
+    system("rm codigo.cpp")
+    system("rm in.txt")
+    out
+  end
 end
