20441e4ef753802e3a91207994f1cd982aaac2be
Adrian Rangel <adrian.rangel@gmail.com>
2014-05-04 23:20:03 -0500

    mostrar errores de compilacion

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,3 +1,4 @@
+require 'Open3'
 class Code < ActiveRecord::Base
   validates_uniqueness_of :titulo
   validates_presence_of :titulo, :codigo
@@ -5,11 +6,22 @@ class Code < ActiveRecord::Base
   def submit
     entrada.to_file("in.txt")
     codigo.to_file("codigo.cpp")
-    system("g++ codigo.cpp -o a.out")
-    out = `./a.out < in.txt`
+
+    stdout_str, stderr_str, status = Open3.capture3("g++ codigo.cpp -o a.out")
+
+    if stderr_str
+      remove_files
+      stderr_str.html_safe
+    else
+      out = `./a.out < in.txt`
+      remove_files
+      out
+    end
+
+  end
+  def remove_files
     system("rm a.out")
     system("rm codigo.cpp")
     system("rm in.txt")
-    out
   end
 end
